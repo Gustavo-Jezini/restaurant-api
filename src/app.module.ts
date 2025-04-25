a507aa01e987f94b552e72824927a6cbfe03687c
Gustavo Jezini <gustavojezini@gmail.com>
2025-04-25 08:52:07 -0300

    feat: create restaurant schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppService } from './app.service';
+import { RestaurantsModule } from './restaurants/restaurants.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.DB_URI_LOCAL || ''),
+    RestaurantsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
